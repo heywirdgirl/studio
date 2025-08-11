4c1945035df1e1ea68b80ab7ef913b6b16c9335c
vincent vm <vincentvm1993@gmail.com>
2025-08-11 01:47:27 +0000

    Now this project i see in viewweb run on vercel or run on local server

--- src/lib/printful-mock-api.ts
+++ src/lib/printful-mock-api.ts
@@ -1,4 +1,2 @@
-import type { Product } from './types';
-
-// This file is now for types and can be renamed or refactored later.
+// This file is now for types and can be refactored later.
 // The actual API calls are in the pages themselves to leverage server components.
